f18be3ea3d2462082d72264d59078e727a3a19a4
marisbotero <maris2143@gmail.com>
2019-05-11 15:55:06 -0500

    💜

--- p5js/sketch.js
+++ p5js/sketch.js
@@ -1,7 +1,12 @@
 function setup() {
-
+  createCanvas(640, 480);
 }
 
 function draw() {
-  ellipse(50, 50, 80, 80);
+  if (mouseIsPressed) {
+    fill(0);
+  } else {
+    fill(255);
+  }
+  ellipse(mouseX, mouseY, 80, 80);
 }
